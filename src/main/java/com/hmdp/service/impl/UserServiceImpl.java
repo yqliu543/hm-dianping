00941466e58d0da4640108262cb8f9050117e2df
lyq <3471966565@qq.com>
2023-03-25 22:56:39 +0800

    543:commit

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
 /**
  * <p>
  * 服务实现类
@@ -44,7 +46,28 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
     @Override
     public Result login(LoginFormDTO loginForm, HttpSession session) {
-        
+        if (RegexUtils.isPhoneInvalid(loginForm.getPhone())){
+            //2.不符合，返回
+            return Result.fail("手机号格式错误");
+        }
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (cacheCode == null||!cacheCode.toString().equals(code)) {
+            return Result.fail("验证码错误");
+        }
+        User user = query().eq("phone", loginForm.getPhone()).one();
+        if (user == null) {
+            user=createUserWithPhone(loginForm.getPhone());
+        }
+        session.setAttribute("user",user);
         return null;
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(10));
+        save(user);
+        return user;
     }
 }
