22fac2140ee290965db7f7dd9f1b94faadeede22
lyq <3471966565@qq.com>
2023-03-27 23:50:40 +0800

    543:commit

---
+++ src/main/java/com/hmdp/utils/CacheClient.java
@@ -0,0 +1,31 @@
+package com.hmdp.utils;
+
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Slf4j
+public class CacheClient {
+    private StringRedisTemplate stringRedisTemplate;
+    public CacheClient(StringRedisTemplate stringRedisTemplate){
+        this.stringRedisTemplate=stringRedisTemplate;
+    }
+    public void set(String key, Object value, Long time, TimeUnit unit){
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(value),time,unit);
+    }
+
+    public void setWithLogicalExpire(String key, Object value, Long time, TimeUnit unit){
+        RedisData redisData = new RedisData();
+        redisData.setData(value);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(unit.toSeconds(time)));
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(redisData));
+    }
+
+
+
+}
