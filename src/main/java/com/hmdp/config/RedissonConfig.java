3962ed2953765cfe7cc2cb6b964daf110a5482ef
lyq <3471966565@qq.com>
2023-04-05 16:39:38 +0800

    543:commit

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -16,7 +16,7 @@ public class RedissonConfig {
     @Bean
     public RedissonClient redissonClient(){
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1");
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
         return Redisson.create(config);
     }
 }
